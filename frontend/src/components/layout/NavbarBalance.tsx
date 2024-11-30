29c3bc9fc7c56ee60c430bf649d260b4fba3d1d9
Daniel Pointon <dlp14developing@gmail.com>
2024-11-30 19:16:04 +0000

    push

--- frontend/src/components/layout/NavbarBalance.tsx
+++ frontend/src/components/layout/NavbarBalance.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import { Balance } from '../../types/balance';
-import { COLORS } from '../../constants/color';
-
 export const NavbarBalance: React.FC<Balance> = ({ currency, amount, icon: Icon }) => {
   const colorClass = currency === 'PRC' ? 'text-yellow-500' : 'text-green-500';
   
   return (
     <div className="flex items-center gap-2">
       <Icon className={colorClass} size={20} />
-      <span className="text-gray-700">
+      <span className="text-white">
         {amount} {currency}
       </span>
     </div>
